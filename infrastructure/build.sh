d4fdca0728b74cc749032ec0c4301c56799a0862
andreas-masaoutis <andreas.masaoutis@protonmail.ch>
2023-06-03 00:33:38 +0300

    deploy: the build script for all images

---
+++ infrastructure/build.sh
@@ -0,0 +1,24 @@
+## The base file with most of the needed software
+docker build \
+  -f Dockerfile_base \
+  -t spark-base .
+
+## The master, mostly exposing ports and entrypoint
+docker build \
+  -f Dockerfile_master \
+  -t spark-master .
+
+## The worker, mostly exposing ports and entrypoint
+docker build \
+  -f Dockerfile_worker \
+  -t spark-worker .
+
+## Different interfaces for interaction, including Jupyter
+docker build \
+  -f Dockerfile_submit \
+  -t spark-submit .
+
+## The MongoDB instance
+docker build \
+  -f Dockerfile_mongo \
+  -t mongo:6.0.6 .
